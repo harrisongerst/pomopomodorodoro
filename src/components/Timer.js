fdfdad69e550a4806bbec7ff9524c10bbc88a22b
harrisongerst <harrisongerstenlauer@gmail.com>
2022-10-27 15:59:59 -0400

    actually not sure what im committing rn

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 export default function Timer() {
+    const [seconds, setSeconds] = useState(1500);
+    const [isActive, setIsActive] = useState(false);
+
+    function toggle(){
+        setIsActive(!isActive);
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+          interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+          }, 1000);
+        } else if (!isActive && seconds !== 0) {
+          clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+      }, [isActive, seconds]);
+
   return (
-    <div>Timer</div>
-  )
+    <div>
+        <h1>Timer</h1>
+    </div>
+  );
 }
