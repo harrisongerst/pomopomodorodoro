88eff8ef63e2d053e917ea81710dc33f68191793
harrisongerst <harrisongerstenlauer@gmail.com>
2022-10-28 15:29:04 -0400

    add onbreak usestate

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 
 export default function Timer() {
-    const [seconds, setSeconds] = useState(1500);
+    const [seconds, setSeconds] = useState(30);
     const [isActive, setIsActive] = useState(false);
+    const [isOnBreak, setIsOnBreak] = useState(false);
 
     function toggle(){
         setIsActive(!isActive);
@@ -12,17 +13,20 @@ export default function Timer() {
         let interval = null;
         if (isActive) {
           interval = setInterval(() => {
-            setSeconds(seconds => seconds + 1);
+            setSeconds(seconds => seconds - 1);
           }, 1000);
+
         } else if (!isActive && seconds !== 0) {
           clearInterval(interval);
-        }
+        } 
         return () => clearInterval(interval);
       }, [isActive, seconds]);
 
   return (
     <div>
-        <h1>Timer</h1>
+        <h1>{isOnBreak ? 'Break' : 'Work'}</h1>
+        <h2>{seconds}</h2>
+        <button onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
     </div>
   );
 }
